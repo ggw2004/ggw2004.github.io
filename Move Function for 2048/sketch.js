b2283a12ee1f138966dfe31f767ea8693a7112cd
ggw2004 <91301141+ggw2004@users.noreply.github.com>
2022-01-09 15:20:29 -0600

    fixed move down function

--- Move Function for 2048/sketch.js
+++ Move Function for 2048/sketch.js
@@ -1,10 +1,12 @@
 let grid = [[2,0,2],
-            [0,0,0],
-            [4,0,2]];
+            [4,0,2],
+            [0,0,0]];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // grid = moveDown(grid);
+  grid = moveDown(grid);
+  grid = moveUp(grid);
+  grid = moveDown(grid);
 }
 
 function draw() {
@@ -32,14 +34,14 @@ function moveUp(grid) {
 }
 
 function moveDown(grid) {
-  for (let y=1; y >= 0; y--) {
+  for (let y=1; y >= 0; y--) {   
     for (let x=2; x >= 0; x--) {
       if (grid[y][x] !== 0) {
-        let thisY = y;
+        let thisY = y;  
         let thisX = x;
-        while (thisY <= 0 && grid[thisY+1][thisX] === 0) { // does not like thisX or thisY
+        while (thisY >= 0 && thisY + 1 <= 2 && grid[thisY+1][thisX] === 0) { // need to add "&& thisY + 1 <=2" in the while loop
           // swap with below
-          grid[thisY + 1 ][thisX] = grid[thisY][thisX];
+          grid[thisY + 1][thisX] = grid[thisY][thisX];
           grid[thisY][thisX] = 0;
 
           //change thisY
